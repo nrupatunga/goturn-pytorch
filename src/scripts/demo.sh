6cc651cca985204783f419266cba377d3f957323
nrupatunga <nrupatunga.tunga@gmail.com>
2020-03-31 23:09:11 +0530

    remove files

--- src/scripts/demo.sh
+++
@@ -1,4 +0,0 @@
-MODEL_DIR='./lightning_logs/version_13/'
-VIDEO_PATH='/home/nthere/Desktop/out.mp4'
-
-python demo.py --model_dir $MODEL_DIR --video_path $VIDEO_PATH
--- src/scripts/test.sh
+++
@@ -1,7 +0,0 @@
-MODEL_DIR='./lightning_logs/version_0/'
-IMAGENET_PATH='/media/nthere/datasets/ISLVRC2014_Det/'
-ALOV_PATH='/media/nthere/datasets/ALOV/'
-
-python test.py --model_dir $MODEL_DIR\
-	--imagenet_path $IMAGENET_PATH \
-	--alov_path $ALOV_PATH
